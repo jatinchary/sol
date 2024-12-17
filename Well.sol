361a5b8b02295c3f95f357bfd7fea1d4377db835
jatinchary <charyjatin@gmail.com>
2024-12-17 19:03:08 +0530

    small smart contract

---
+++ Well.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Will{
+    uint startTime;
+    uint tenYears;
+    uint public lastVisited;
+    address owner;
+    address  payable recipient;
+   
+   constructor(address payable _recipient ){
+    recipient= _recipient;
+    tenYears=10;
+    // 1 hours*24 *365*10;
+    startTime= block.timestamp;
+    lastVisited=block.timestamp;
+    owner=msg.sender;
+   }
+
+   modifier onlyOwner(){
+    require(owner == msg.sender);
+        _; 
+    }
+    modifier onlyRecipent(){
+        require(msg.sender == recipient);
+        _; 
+    }
+  function deposit()public payable onlyOwner {
+    lastVisited= block.timestamp;
+
+  }
+  function ping() public onlyOwner {
+    lastVisited=block.timestamp;
+  }
+  function claim() external onlyRecipent {
+    require(lastVisited<block.timestamp-tenYears);
+    payable (recipient).transfer(address(this).balance);
+  }
+
+
+}
